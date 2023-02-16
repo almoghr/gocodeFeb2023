62df714d8ed7da66791d303b94781bb9a4939338
almoghr <almoghr36@gmail.com>
2023-02-16 18:05:15 +0200

    lots of js code

---
+++ js-basics/main.js
@@ -0,0 +1,75 @@
+// console.log('hello world');
+
+// const myName = 'almog'; // string
+// const myName2 = `almog`;
+// console.log(myName,myName2);
+// const myAge = 31; // number
+// const omarAge = 30;
+// const yudiAge = '21';
+// console.log(myAge);
+// const isMale = true; // boolean
+// const isFemale = false; // boolean
+
+// console.log(isMale, isFemale);
+// let favoriteFood = undefined;
+
+// console.log(favoriteFood);
+// //{} object // [] array // function() // 
+
+// favoriteFood = 'pizza';
+// console.log(favoriteFood);
+
+// console.log(3+6);
+// console.log(3*6);
+// console.log(2*(5+5));
+// console.log(6/3);
+// console.log(6-3);
+// console.log(6**3);
+// console.log(3+5*8); // 43 // 64
+// console.log(myAge + omarAge);
+// console.log(yudiAge + omarAge);
+
+// console.log(myAge+omarAge+yudiAge); // '6121'
+
+// console.log(yudiAge+myAge+omarAge); // ''
+// console.log(yudiAge+(myAge+omarAge)); // ''
+
+// console.log(yudiAge+myAge*omarAge);
+
+// let counter = 0; // 0
+
+// console.log(counter);
+
+// counter++; // 1
+// console.log(counter);
+// counter = counter + 1; // 2
+// console.log(counter);
+// counter--; // 1
+// console.log(counter);
+// counter = counter - 1; // 0
+// console.log(counter);
+
+// >, >=, <, <=, ===, !==
+
+// const a = 'something'
+
+// console.log(3>6)
+// console.log(10 >= 5+2)
+// console.log(3<6)
+// console.log(10 <= 5+2)
+// console.log(10 === 5+2)
+// console.log(9*9 !== 82)
+// console.log(9*9 !== 81)
+
+// console.log(3 === '3')
+// console.log(3 == '3') // לא לשימוש
+// console.log(3 !== '3')
+// console.log(3 != '3')
+
+// let myMoney = 100
+// const mySalary = 25000
+// console.log(myMoney)
+// myMoney = myMoney + mySalary
+// console.log(myMoney)
+
+// myMoney += mySalary
