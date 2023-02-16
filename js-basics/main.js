d72ee6b4cfaf378a6102e4c06d928355a2a5509f
almoghr <almoghr36@gmail.com>
2023-02-16 20:41:21 +0200

    loops

--- js-basics/main.js
+++ js-basics/main.js
@@ -3,8 +3,8 @@
 // const myName = 'almog'; // string
 // const myName2 = `almog`;
 // console.log(myName,myName2);
-// const myAge = 31; // number
-// const omarAge = 30;
+const myAge = 31; // number
+const omarAge = 60;
 // const yudiAge = '21';
 // console.log(myAge);
 // const isMale = true; // boolean
@@ -74,4 +74,97 @@
 
 // myMoney += mySalary
 
+// if(myAge > omarAge) {
+//     console.log(' i am bigger than Omar')
+// } else{
+//     console.log('Omar is bigger than me')
+// }
+
+// const myWeight=95
+// const myWeight2=85
+// const myWeight3=70
+
+// if(myWeight < myWeight2){
+//     console.log(myWeight)
+// } else if(myWeight > myWeight3){
+//     console.log('got into else if')
+// } else{
+//     console.log(myWeight3)
+// }
+
+// const whatIsYourName = prompt('enter your name')
+
+// console.log(whatIsYourName)
+
+// const whatDayIsToday = prompt('tell me the day')
+// const isAlmogHappy = true
+// const isHoliday = false
+
+// if(whatDayIsToday === 'sunday'){
+//     console.log('today is sunday')
+//     if(isAlmogHappy === false){
+//         console.log('sunday has came and there is a whole wwek of work')
+//     } 
+// } else if(whatDayIsToday === 'monday' && isHoliday === false){
+//     console.log('today is monday, go to work')
+// }else if(whatDayIsToday === 'tuesday'){
+//     console.log('today is tuesday')
+// }else if(whatDayIsToday === 'wednesday'){
+//     console.log('today is wednesday')
+// }else if(whatDayIsToday === 'thursday'){
+//     console.log('today is thursday')
+// }else if(whatDayIsToday === 'friday'){
+//     console.log('today is friday and a vacation')
+// }else if(whatDayIsToday === 'saturday'){
+//     console.log('today is saturday')
+//     if(isAlmogHappy === true || isHoliday === true){
+//         console.log('lets dance')
+//     }
+// } else{
+//     alert('there is no such day')
+// }
+
+// console.log('done')
+
+// console.log('get cup')
+// console.log('fill cup')
+// console.log('put choclate marbles')
+// console.log('put long ornament')
+
+for(let i = 1; i <= 30; i = i+1){
+    console.log('get cup')
+    console.log('fill cup')
+    console.log('put choclate marbles')
+    console.log('put long ornament')
+}
+
+let line = ''
+
+for(let i = 1; i <= 10; i++){
+   line += i*8 + ' '
+   console.log(line)
+}
+
+
+for(let j=1; j<=10; j++){
+    let line2 = ''
+    for(let k=1;k<=10;k++){
+        line2 += j * k + '\t';
+    }
+    console.log(line2)
+}
+
+
+let x = 11;
+while(x < 10){
+    console.log('i am only less than 10 years old')
+    x++
+}
+
+console.log(x)
+do{
+    console.log('i am only less than 10 years old')
+    x++
+} while(x<10)
+console.log(x)
 
