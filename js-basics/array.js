41bc5f384ace164d25d9c3613041c47918b8e259
almoghr <almoghr36@gmail.com>
2023-03-09 20:02:24 +0200

    answered objects + methods of array learnt

--- js-basics/array.js
+++ js-basics/array.js
@@ -1,33 +1,210 @@
-const arr = [1, 2, 3, 4, 5, 6];
-const strArr = ["almog", "brad", "carl"];
-const arrArr = [["almog", "sammy"], ["brad", "ben"], ["carl", "candy"]];
-const objArr = [
-  { name: "almog" },
-  { name: "almog" },
-  { name: "almog" },
-  { name: "almog" },
-  { name: "almog" },
-  { name: "almog" },
-  { name: "almog" },
-  { name: "almog" },
-  { name: "almog" },
-  { name: "almog" },
-  { name: "almog" },
-  { name: "almog" },
+// const arr = [1, 2, 3, 4, 5, 6];
+// const strArr = ["almog", "brad", "carl"];
+// const arrArr = [["almog", "sammy"], ["brad", "ben"], ["carl", "candy"]];
+// const objArr = [
+//   { name: "almog" },
+//   { name: "almog" },
+//   { name: "almog" },
+//   { name: "almog" },
+//   { name: "almog" },
+//   { name: "almog" },
+//   { name: "almog" },
+//   { name: "almog" },
+//   { name: "almog" },
+//   { name: "almog" },
+//   { name: "almog" },
+//   { name: "almog" },
+// ];
+// const mixedArr = [1, 8, "a", 5, true, false, {}, { name: "almog" }];
+
+// console.log(arr[0], arr[arr.length-1])
+
+// console.log(strArr[0][3],strArr[1],strArr[2])
+// console.log(arrArr[0][1][4])
+
+// //arrArr[0] = ["almog", "sammy"]
+// //arrArr[0][1] = "sammy"
+// //arrArr[0][1][4] = "y"
+
+// console.log(strArr.length)
+// console.log(strArr[strArr.length-1])
+// for(let i = 0; i < strArr.length; i++){
+//     console.log(strArr[i][0])
+// }
+
+//array methods
+
+const fruitsArr = [
+  "Apple",
+  "Banana",
 ];
-const mixedArr = [1, 8, "a", 5, true, false, {}, { name: "almog" }];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+fruitsArr.forEach(function (item, index) {
+  console.log(item, index);
+});
 
-console.log(arr[0], arr[arr.length-1])
+numbers.forEach(function (num) {
+  console.log(num ** 4);
+});
 
-console.log(strArr[0][3],strArr[1],strArr[2])
-console.log(arrArr[0][1][4])
+//foreach is good for actions on the array elements
+//but NOT for changing the array and getting a new one instead!
 
-//arrArr[0] = ["almog", "sammy"]
-//arrArr[0][1] = "sammy"
-//arrArr[0][1][4] = "y"
+const newNumbersArr = numbers.map(function (num) {
+  return num ** 4;
+});
 
-console.log(strArr.length)
-console.log(strArr[strArr.length-1])
-for(let i = 0; i < strArr.length; i++){
-    console.log(strArr[i][0])
-}
+console.log(newNumbersArr, numbers);
+// map is working like forEach but returns a value
+// the value is a new array with the modified elements
+
+const filteredNumbers = numbers.filter(function (item) {
+  return item > 5;
+});
+
+console.log(filteredNumbers);
+
+// filter is working like forEach but returns a value
+// that answers true to the specific condition we gave
+
+const foundNumber = numbers.find(function (item) {
+    return item > 5;
+  })
+
+  const foundIndex = numbers.findIndex(function (item) {
+    return item > 5;
+  })
+
+  console.log(foundNumber, foundIndex);
+
+//find is working like filter but stops after
+// the first true statement it finds and returns the item
+//findIndex is doing the exact same thing but return the index of the item
+
+const arr = ['hello','there','my','name','is']
+
+const s = arr.push('almog')
+const u = arr.unshift('welcome and')
+console.log(arr, s, u)
+
+//push is a method that inserts an element to the end of the array
+//unshift is a method that inserts an element to the start of the array
+
+const p = arr.pop()
+const sh = arr.shift()
+console.log(arr, p, sh)
+
+//pop is a method that removes the last element of the array and returns it
+//shift is a method that removes the first element of the array and returns it
+
+const fruits2 = ['Apple', 'Banana', 'Orange', 'Orange'];
+
+const indFruits2 = fruits2.indexOf("Orange")
+const ind2Fruits2 = fruits2.indexOf("an")
+const ind3Fruits2 = fruits2.includes("Orange")
+const ind4Fruits2 = fruits2.includes("an")
+
+
+console.log(indFruits2, ind2Fruits2, ind3Fruits2, ind4Fruits2)
+
+//indexOf, like string, will return the index of the element in the array
+//if nothing will be found then returns -1
+//includes, like string, will return a true statement if the element is in the array
+//if nothing will be found then returns false
+
+const newNumbers = numbers.slice(3, 7)
+const copyNumbers = numbers.slice()
+
+console.log(newNumbers, numbers, copyNumbers)
+
+//slice is a function that lets you get a portion 
+//of the array without modifying the array itself
+
+const notGoodCopyArr = numbers
+console.log(notGoodCopyArr,numbers)
+
+notGoodCopyArr.push('hello')
+console.log(notGoodCopyArr,numbers)
+
+// exmaple for a save by reference with arrays - should use slice \ whatever we will learn sooner or later
+
+const splicedNumbers = numbers.splice(2, 8)
+
+console.log(splicedNumbers, numbers)
+
+//splice is a function that lets you get a portion 
+//of the array and modifies the array itself
+
+const firstArr = [1,2,3]
+const secondArr = [4,5,6]
+const thirdArr = [7,8,9]
+
+
+const newArr = firstArr.concat(secondArr, thirdArr)
+
+console.log(newArr)
+
+//concat literally concatanates array together and returns the new array
+
+
+const elements = ['Fire', 'Wind', 'Rain'];
+
+console.log(elements.join());
+// expected output: Fire,Wind,Rain
+
+console.log(elements.join(''));
+// expected output: FireWindRain
+
+console.log(elements.join('-'));
+// expected output: Fire-Wind-Rain
+
+console.log(elements.join('*'));
+// expected output: Fire*Wind*Rain
+
+console.log(numbers.join('+'))
+
+//join turns an array to a string with the specified seperator.
+
+const date = '22/02/2023:08:45:06'
+
+const dateWithoutHour = date.split(':')
+console.log(dateWithoutHour[0])
+
+const splittedDate = dateWithoutHour[0].split('/')
+console.log(splittedDate)
+
+//split turns a string to an array and divides by the specified seperator.
+
+console.log(newArr)
+
+newArr.reverse()
+
+console.log(newArr)
+
+// reverse turns all the items in the array from the end to the beginning
+// it is mutating the original array
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- js-basics/objects.js
+++ js-basics/objects.js
@@ -1,77 +1,169 @@
-const person = {
-    name:'almog',
-    age: 31,
-    mail: 'almoghr36@gmail.com',
-    sayHello: function(){
-        console.log('heeeeyyyyyy')
-    },
-    job: {
-        title:'ceo',
-        salary:'50k'
-    }
-}
+// const person = {
+//     name:'almog',
+//     age: 31,
+//     mail: 'almoghr36@gmail.com',
+//     sayHello: function(){
+//         console.log('heeeeyyyyyy')
+//     },
+//     job: {
+//         title:'ceo',
+//         salary:'50k'
+//     }
+// }
 
-console.log(person)
+// console.log(person)
 
-console.log(person.name, person.age)
-console.log(person['name'], person['age'])
-console.log(person.job)
-console.log(person.job.title)
-console.log(person.job.salary)
-console.log(person['job'])
-console.log(person['job']['title'])
-console.log(person['job'].salary)
-person.sayHello()
+// console.log(person.name, person.age)
+// console.log(person['name'], person['age'])
+// console.log(person.job)
+// console.log(person.job.title)
+// console.log(person.job.salary)
+// console.log(person['job'])
+// console.log(person['job']['title'])
+// console.log(person['job'].salary)
+// person.sayHello()
 
 
-const jobRequirements = {
-    front:{
-        framework:'angular',
-        lang:'js',
-        engine:'node'
-    },
-    back:{
-        runtime:'nodejs',
-        db:'mongoose',
-        server:'express'
-    },
+// const jobRequirements = {
+//     front:{
+//         framework:'angular',
+//         lang:'js',
+//         engine:'node'
+//     },
+//     back:{
+//         runtime:'nodejs',
+//         db:'mongoose',
+//         server:'express'
+//     },
     
-}
+// }
 
-function printWhateverIWantFromTheObject(key1,key2){
-    console.log(jobRequirements[key1][key2])
-    // console.log(jobRequirements.key1.key2) // לא תקין
-}
+// function printWhateverIWantFromTheObject(key1,key2){
+//     console.log(jobRequirements[key1][key2])
+//     // console.log(jobRequirements.key1.key2) // לא תקין
+// }
 
-console.log(jobRequirements['back']['db'])
+// console.log(jobRequirements['back']['db'])
 
-printWhateverIWantFromTheObject('back', 'db')
+// printWhateverIWantFromTheObject('back', 'db')
 
 
-person.name = 'david'
-person.age = 21;
-person.job.title ='DEVELOPER'
-console.log(person)
+// person.name = 'david'
+// person.age = 21;
+// person.job.title ='DEVELOPER'
+// console.log(person)
 
-person.lastName = 'shukrun'
-person.pet = {
-    race:'bulldog',
-    color:'blue'
-}
-person.job.dayOff = 'friday'
-console.log(person)
+// person.lastName = 'shukrun'
+// person.pet = {
+//     race:'bulldog',
+//     color:'blue'
+// }
+// person.job.dayOff = 'friday'
+// console.log(person)
 
-const person2 = {
-    name:'almog',
-    age: 31,
-    mail: 'almoghr36@gmail.com',
-    sayHello: function(){
-        console.log('heeeeyyyyyy')
-    },
-}
+// const person2 = {
+//     name:'almog',
+//     age: 31,
+//     mail: 'almoghr36@gmail.com',
+//     sayHello: function(){
+//         console.log('heeeeyyyyyy')
+//     },
+// }
 
-for (const key in person2) {
-    console.log(key + ':' + person2[key]);
-  }
+// for (const key in person2) {
+//     console.log(key + ':' + person2[key]);
+//   }
 
-//   console.log(person['2']) 
+// //   console.log(person['2']) 
+
+// console.log(Object.keys(person))
+// console.log(Object.keys(person).length)
+
+
+
+//HOMEWORK
+
+
+// //EX1
+
+// const cat = {
+//     color:'black',
+//     name:'mitzi',
+//     sayMeow: function(){
+//         console.log('mmmmmeeeeoooowwwW!')
+//     }
+// }
+// console.log(cat)
+
+// //EX2 
+
+// const nameOfProperty = prompt("choose a property you would like to add")
+// const valueOfProperty = prompt("choose a value you world like to add to the property")
+
+// cat[nameOfProperty] = valueOfProperty
+// console.log(cat)
+//EX3
+
+// function validateName(catObj, catName){
+//     return (catObj.name === catName)
+// }
+
+// console.log(validateName(cat, "shmil"))
+// console.log(validateName(cat, "mitzi"))
+
+//EX4
+
+// const objOne = {
+//     a:1,
+//     b:2,
+//     c:3,
+// }
+
+// const objTwo = {
+//     d:4,
+//     e:5,
+//     f:6,
+// }
+
+// function combineObjects(obj1, obj2){
+//     const mergedObject = {};
+//     for(const key in obj1){
+//         mergedObject[key] = obj1[key]
+//     }
+//     for(const key in obj2){
+//         mergedObject[key] = obj2[key]
+//     }
+//     return mergedObject
+// }
+
+// combineObjects(objOne,objTwo)
+
+
+//EX5
+
+// const dogs = {}
+
+// for(let i = 0; i < 3; i++){
+//     const dogNum = 'dog'+(i+1)
+//     dogs[dogNum] = {}
+//     dogs[dogNum]['color'] = prompt("")
+// }
+
+// console.log(dogs)
+
+//EX6
+
+
+// const fishes = {}
+
+// for(let i = 0; i < 3; i++){
+//     const fishNum = 'fish'+(i+1)
+//     fishes[fishNum] = {}
+//     fishes[fishNum]['name'] = prompt("")
+// }
+// console.log(fishes)
+// const chosenName = prompt("enter any name")
+
+// for (const key in fishes){
+//     console.log(validateName(fishes[key], chosenName))
+// }
